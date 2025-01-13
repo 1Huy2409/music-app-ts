da31e184eb1561875cd0f1526c58ce586ad1ce6b
1Huy2409 <nhathuy2409@gmail.com>
2025-01-13 11:18:57 +0700

    delete console

--- controller/admin/upload.controller.ts
+++ controller/admin/upload.controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express';
 export const index = async (req: Request, res: Response) : Promise<void> =>
 {
-    console.log(req.body);
     res.json(
         {
             location: req.body.file,
